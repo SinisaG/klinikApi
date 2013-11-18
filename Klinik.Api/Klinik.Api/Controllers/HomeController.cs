9f73494a49f6becb7a74445767fffa39400170a7
Sinisa <sinisa@hackandcraft.com>
2013-11-18 19:15:36 +0100

    initail commit

---
+++ Klinik.Api/Klinik.Api/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using HackandCraft.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Klinik.Api.Controllers
+{
+    public class HomeController : HackandCraftController
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
