d7bb2558ced5e7421d24a0063c1963fa1eec141f
Sinisa <sinisa@hackandcraft.com>
2013-12-05 11:08:05 +0100

    image and text

---
+++ Klinik.Models/Image.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Klinik.Models
+{
+    [Serializable]
+    public class Image
+    {
+        [XmlAttribute]
+        public int id { get; set; }
+        [XmlAttribute]
+        public string url { get; set; }
+    }
+}
---
+++ Klinik.Models/Text.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Klinik.Models
+{
+    [Serializable]
+    public class Text
+    {
+        [XmlAttribute]
+        public int id { get; set; }
+        [XmlAttribute]
+        public string title { get; set; }
+        [XmlAttribute]
+        public string body { get; set; }
+    }
+}
